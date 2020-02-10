4d8844c54109acacb535bf5349b50326d76d5936
申鸿毓 <shenhongyu@shenhongyudeMacBook-Pro.local>
2020-02-10 01:55:41 -0600

    code

---
+++ reactProjectTest/ClientApp/src/components/Counter.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { IApplicationState } from '../store';
+import * as CounterStore from '../store/Counter';
+import { Dispatch } from 'redux';
+
+//type CounterProps =
+//    CounterStore.CounterState &
+//    typeof CounterStore.actionCreators &
+//    RouteComponentProps<{}>;
+
+type CounterProps = ICounterStateProps & ICounterDispatchProps;
+
+interface ICounterStateProps {
+    count: number;
+}
+
+interface ICounterDispatchProps {
+    setCount: (count: number) => void;
+    increment: () => void;
+    decrement: () => void;
+}
+
+class Counter extends React.PureComponent<CounterProps> {
+    public render() {
+        return (
+            <React.Fragment>
+                <h1>Counter</h1>
+
+                <p>This is a simple example of a React component.</p>
+
+                <p aria-live="polite">Current count: <strong>{this.props.count}</strong></p>
+
+                <button type="button"
+                    className="btn btn-primary btn-lg"
+                    onClick={() => { this.props.increment(); }}>
+                    Increment
+                </button>
+            </React.Fragment>
+        );
+    }
+};
+
+const mapStateToProps = (state: IApplicationState): ICounterStateProps => {
+    return {
+        count: state.counter.count
+    }
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<CounterStore.ConterAction>): ICounterDispatchProps => {
+    return {
+        setCount: (count: number) => dispatch(CounterStore.actionCreators.setCount(count)),
+        increment: () => dispatch(CounterStore.actionCreators.increment()),
+        decrement: () => dispatch(CounterStore.actionCreators.decrement()),
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Counter);
---
+++ reactProjectTest/ClientApp/src/store/Counter.ts
@@ -0,0 +1,63 @@
+import { Action, Reducer } from 'redux';
+
+// -----------------
+// STATE - This defines the type of data maintained in the Redux store.
+
+export interface CounterState {
+    count: number;
+}
+
+enum ActionType {
+    IncrementCount = "INCREMENT_COUNT",
+    DecrementCount = "DECREMENT_COUNT",
+    SetCount = "SET_COUNT",
+}
+
+// -----------------
+// ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
+// They do not themselves have any side-effects; they just describe something that is going to happen.
+// Use @typeName and isActionType for type detection that works even after serialization/deserialization.
+
+interface IncrementCountAction { type: ActionType.IncrementCount }
+interface DecrementCountAction { type: ActionType.DecrementCount }
+
+interface SetCountAction {
+    type: ActionType.SetCount;
+    count: number;
+}
+
+
+// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
+// declared type strings (and not any other arbitrary string).
+export type ConterAction = IncrementCountAction | DecrementCountAction | SetCountAction;
+
+// ----------------
+// ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
+// They don't directly mutate state, but they can have external side-effects (such as loading data).
+
+export const actionCreators = {
+    increment: (): IncrementCountAction => ({ type: ActionType.IncrementCount }),
+    decrement: (): DecrementCountAction => ({ type: ActionType.DecrementCount }),
+    setCount: (newCount: number): SetCountAction => ({ type: ActionType.SetCount, count: newCount }),
+};
+
+// ----------------
+// REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
+
+export const reducer: Reducer<CounterState> = (state: CounterState | undefined, action: ConterAction): CounterState => {
+    if (state === undefined) {
+        return { count: 0 };
+    }
+
+    //const action = incomingAction as ConterAction;
+    switch (action.type) {
+        case 'INCREMENT_COUNT':
+            return { count: state.count + 1 };
+        case 'DECREMENT_COUNT':
+            return { count: state.count - 1 };
+        case ActionType.SetCount:
+            return { count: action.count };
+        default:
+            return state;
+    }
+};
