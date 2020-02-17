56071462c43c1dea3c8ad28b44a4047374af9796
申鸿毓 <shenhongyu@shenhongyudembp.dyndns.rice.edu>
2020-02-17 15:56:26 -0600

    updated

--- reactProjectTest/ClientApp/src/store/Counter.ts
+++ reactProjectTest/ClientApp/src/store/Counter.ts
@@ -29,7 +29,7 @@ interface SetCountAction {
 
 // Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
 // declared type strings (and not any other arbitrary string).
-export type ConterAction = IncrementCountAction | DecrementCountAction | SetCountAction;
+export type CounterAction = IncrementCountAction | DecrementCountAction | SetCountAction;
 
 // ----------------
 // ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
@@ -44,7 +44,7 @@ export const actionCreators = {
 // ----------------
 // REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
 
-export const reducer: Reducer<CounterState> = (state: CounterState | undefined, action: ConterAction): CounterState => {
+export const reducer: Reducer<CounterState> = (state: CounterState | undefined, action: CounterAction): CounterState => {
     if (state === undefined) {
         return { count: 0 };
     }
---
+++ reactProjectTest/ClientApp/src/store/Modules.ts
@@ -0,0 +1,31 @@
+﻿import { Action, Reducer } from 'redux';
+
+
+
+export interface ModulesState {
+}
+
+enum ActionType {
+}
+
+// ACTIONS
+
+
+
+// ACTION CREATERS
+
+export const actionCreators = {
+
+};
+
+// ----------------
+// REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
+
+export const reducer: Reducer<ModulesState> = (state: ModulesState | undefined, action: ConterAction): ModulesState => {
+    if (state === undefined) {
+        return {
+            /* Defualt state */
+        };
+    }
+
+};
